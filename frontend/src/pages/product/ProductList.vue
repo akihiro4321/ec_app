b023512f070f7037736c5f83923b0a188a143e55
akihiro4321 <akihiro.okuya@gmail.com>
2023-09-17 12:00:28 +0900

    商品のキーワード検索機能を追加

--- frontend/src/pages/product/ProductList.vue
+++ frontend/src/pages/product/ProductList.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
     <section>
-      <h2>Filter</h2>
+      <div class="search-container">
+        <input
+          class="search-input"
+          type="text"
+          name="query"
+          placeholder="Search..."
+          v-model.trim="keyword"
+        />
+        <button class="search-button" @click="filterByKeyword">Search</button>
+      </div>
     </section>
     <section>
       <h2>All Products</h2>
       <product-item
-        v-for="product in getProducts"
+        v-for="product in selectedProducts"
         :key="product.productId"
         :id="product.productId"
         :product-name="product.productName"
@@ -24,20 +33,64 @@ export default {
   components: {
     ProductItem,
   },
-  computed: {
-    getProducts() {
-      return this.$store.getters['products/products'];
-    },
+  data() {
+    return {
+      selectedProducts: [],
+      keyword: '',
+    };
   },
   created() {
     this.loadProducts();
+    this.selectedProducts = this.$store.getters['products/products'];
   },
   methods: {
     async loadProducts() {
       await this.$store.dispatch('products/loadProducts');
     },
+    filterByKeyword() {
+      if (this.keyword === '') {
+        this.resetFilter();
+        return;
+      }
+      const filteredProducts = this.selectedProducts.filter((product) => {
+        return product.productName
+          .toLowerCase()
+          .includes(this.keyword.toLowerCase());
+      });
+      this.selectedProducts = filteredProducts;
+    },
+    resetFilter() {
+      this.selectedProducts = this.$store.getters['products/products'];
+    },
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.search-container {
+  text-align: center;
+  margin: 50px auto;
+}
+
+.search-input {
+  padding: 10px;
+  width: 300px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  font-size: 1rem;
+}
+
+.search-button {
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  font-size: 1rem;
+  cursor: pointer;
+}
+
+.search-button:hover {
+  background-color: #0056b3;
+}
+</style>
